d93b7d98922f8eefe34837abf085839c0faed3b6
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2026-03-25 11:09:17 -0500

    DEX-17 - Restructure

---
+++ src/api.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+
+import pokedex from "./pokedex";
+
+const api = new Hono();
+
+api.get("/v1/pokemon/:id", async (c) => {
+  const { id } = c.req.param();
+  const pokemonId = parseInt(id);
+
+  if (isNaN(pokemonId)) return c.text("Invalid Pokemon ID.", 400);
+
+  const pokemon = await pokedex.getPokemonById(pokemonId);
+
+  return pokemon ? c.json(pokemon) : c.text("Pokemon not found.", 404);
+});
+
+api.get("/v1/pokemon", async (c) => {
+  const { name } = c.req.query();
+
+  if (!name) return c.text("Query parameter name= is required.", 400);
+
+  const pokemon = await pokedex.getPokemonByName(name);
+
+  return pokemon ? c.json(pokemon) : c.text("Pokemon not found.", 404);
+});
+
+export default api;
--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,13 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 
 import gql from "./gql";
-import pokedex from "./pokedex";
+import api from "./api";
 
 const app = new Hono();
 
 app.use(logger());
-
 app.use("/graphql", async (c) => gql.handle(c.req.raw));
-
-app.get("/api/v1/pokemon/:id", async (c) => {
-  const { id } = c.req.param();
-  const pokemonId = parseInt(id);
-
-  if (isNaN(pokemonId)) return c.text("Invalid Pokemon ID.", 400);
-
-  const pokemon = await pokedex.getPokemonById(pokemonId);
-
-  return pokemon ? c.json(pokemon) : c.text("Pokemon not found.", 404);
-});
-
-app.get("/api/v1/pokemon", async (c) => {
-  const { name } = c.req.query();
-
-  if (!name) return c.text("Query parameter name= is required.", 400);
-
-  const pokemon = await pokedex.getPokemonByName(name);
-
-  return pokemon ? c.json(pokemon) : c.text("Pokemon not found.", 404);
-});
+app.route("/api", api);
 
 export default {
   fetch: app.fetch,
