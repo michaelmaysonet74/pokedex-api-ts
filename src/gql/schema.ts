7f9488c288f578e4d874163eb84b5049fbc6f095
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2026-03-25 21:18:24 -0500

    Formatting

--- src/gql/schema.ts
+++ src/gql/schema.ts
@@ -1,55 +1,55 @@
 export const typeDefs = `
-type Pokemon {
-  id: Int!
-  name: String!
-  abilities: [Ability!]!
-  baseStats: BaseStats!
-  category: String!
-  entry: String!
-  evolution: EvolutionChain!
-  generation: Int!
-  measurement: Measurement!
-  sprite: String!
-  types: [String!]!
-  immunities: [String!]!
-  resistances: [String!]!
-  weaknesses: [String!]!
-}
+  type Pokemon {
+    id: Int!
+    name: String!
+    abilities: [Ability!]!
+    baseStats: BaseStats!
+    category: String!
+    entry: String!
+    evolution: EvolutionChain!
+    generation: Int!
+    measurement: Measurement!
+    sprite: String!
+    types: [String!]!
+    immunities: [String!]!
+    resistances: [String!]!
+    weaknesses: [String!]!
+  }
 
-type Ability {
-  name: String!
-  effect: String
-  isHidden: Boolean!
-}
+  type Ability {
+    name: String!
+    effect: String
+    isHidden: Boolean!
+  }
 
-type BaseStats {
-  hp: Int!
-  attack: Int!
-  defense: Int!
-  specialAttack: Int!
-  specialDefense: Int!
-  speed: Int!
-}
+  type BaseStats {
+    hp: Int!
+    attack: Int!
+    defense: Int!
+    specialAttack: Int!
+    specialDefense: Int!
+    speed: Int!
+  }
 
-type EvolutionChain {
-  from: Evolution
-  to: [Evolution!]
-}
+  type EvolutionChain {
+    from: Evolution
+    to: [Evolution!]
+  }
 
-type Evolution {
-  id: ID!
-  name: String!
-}
+  type Evolution {
+    id: ID!
+    name: String!
+  }
 
-type Measurement {
-  height: String!
-  weight: String!
-}
+  type Measurement {
+    height: String!
+    weight: String!
+  }
 
-type Query {
-  pokemonById(id: Int!): Pokemon
-  pokemonByName(name: String!): Pokemon
-}
+  type Query {
+    pokemonById(id: Int!): Pokemon
+    pokemonByName(name: String!): Pokemon
+  }
 `;
 
 export default typeDefs;
